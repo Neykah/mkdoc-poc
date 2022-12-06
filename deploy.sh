06332efaeb31ad7c08704ca1cbea4c518992c7f3
Morgan Lefranc <mlefranc@ridge-i.com>
2022-12-06 22:57:18 +0900

    Encryption

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
+if [ -z "$SECRET" ]; then
+    echo "Error: SECRET is not set!"
+    exit 125
+fi
 pip install poetry
 poetry install --with docs
 cd docs
 poetry run mkdocs build
+npm install -g staticrypt
+find site -type f -name "*.html" -exec staticrypt {} $SECRET -o {} \;
